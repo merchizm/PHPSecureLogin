2c8505b7dbf141b8d3a551e9fd96c331ddcf5548
Meriç Enes <52634652+merchizm@users.noreply.github.com>
2022-05-24 19:01:54 +0300

    readme.md updated & check_auth_code_and_login function added.

--- lib/user.class.php
+++ lib/user.class.php
@@ -4,7 +4,6 @@ namespace Rocks;
 
 require_once __DIR__ . "/database.class.php";
 
-use Exception;
 use Rocks\Authenticator;
 
 enum Authority: int
@@ -39,6 +38,7 @@ enum AccountStatus: int
 
 class User
 {
+    private \Rocks\Authenticator $auth;
 
     /**
      * @throws RocksException
@@ -47,22 +47,23 @@ class User
         private readonly Database $db
     )
     {
+        $this->auth = new Authenticator();
         if (!$this->db->checkConnection())
             throw new RocksException('Database connection not established.', code: 3);
     }
 
     /**
-     * Authenticate the user.
+     * Authenticate the user and create temporary credentials.
      * @param $username
      * @param $password
      * @return bool
      */
-    function auth($username, $password): bool
+    public function auth($username, $password): bool
     {
         $user = $this->db->where("_users", "username", $username);
         if (password_verify($password, $user["password"])) {
             session_regenerate_id();
-            $_SESSION['time'] = time();
+            $_SESSION['time'] = date_create();
             $_SESSION['temp_user'] = $user["username"];
             $_SESSION['temp_code'] = $user["2fa_auth_code"];
             return true;
@@ -75,25 +76,60 @@ class User
      * Destroy the user session.
      * @return bool
      */
-    function logout() : bool
+    public function logout() : bool
     {
         return session_destroy();
     }
 
-    function login(){
-        $user = $_SESSION['temp_user'];
+    /**
+     * Check 2FA code and log in.
+     * @param $code
+     * @param $remember
+     * @return bool|string
+     */
+    public function check_auth_code_and_login($code, $remember): bool|string
+    {
+        if($this->auth->verify_code($_SESSION['temp_code'], $code)){
+            $date_diff = date_diff($_SESSION['time'], date_create());
+            $minutes = $date_diff->days * 24 * 60;
+            $minutes += $date_diff->h * 60;
+            $minutes += $date_diff->i;
+            if($minutes < 5){
+                $username = $_SESSION['temp_user'];
+                session_regenerate_id();
+                $_SESSION['user'] = $username;
+                $_SESSION['logged_in'] = true;
+                $_SESSION['time'] = date_create();
+                if($remember)
+                    $this->remember($username);
+                return json_encode([
+                    'status' => 'success',
+                    'message' => 'You\'re successfully logged in.'
+                ]);
+            }else{
+                return json_encode([
+                    'status' => 'error',
+                    'message' => 'Your session has expired, please refresh the page and try again.'
+                ]);
+            }
+        } else {
+            return json_encode([
+                'status' => 'error',
+                'message' => 'Your credentials didn\'t match. Please check your username and password.'
+            ]);
+        }
     }
 
-    function check_auth_code() : bool
+    private function remember($username)
     {
-        return false;
+        // TODO: make this ;)
     }
 
     /**
      * Returns end user IP Address
      * @return string
      */
-    function get_user_ip_address(): string
+    private function get_ip_address(): string
     {
         if (!empty($_SERVER['HTTP_CLIENT_IP']))
             $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -112,12 +148,12 @@ class User
      * @return bool
      * @throws RocksException
      */
-    function create(array $data, Authority $authority, AccountStatus $status): bool
+    public function create(array $data, Authority $authority, AccountStatus $status): bool
     {
         return $this->db->insert("_users", [
             ...$data,
             "registry_date" => date("Y-m-d H:i:s"),
-            "registry_ip_address" => $this->get_user_ip_address(),
+            "registry_ip_address" => $this->get_ip_address(),
             "2fa_auth_code" => Authenticator::generate_random_secret(),
             "2fa_backup_code" => rand("112121", "999999"),
             "authority" => $authority->value,
--- readme.md
+++ readme.md
@@ -1,4 +1,34 @@
 # PHPSecureLogin
 [![CodeFactor](https://www.codefactor.io/repository/github/merchizm/phpsecurelogin/badge)](https://www.codefactor.io/repository/github/merchizm/phpsecurelogin)
+[![PHP version](https://img.shields.io/badge/php-%3E%3D%208.1-8892BF.svg)](https://github.com/phpredis/phpredis)
 
-Description to be entered soon.
+By this project, you will have a more secure and strong authentication system. 
+
+The project runs on PHP 8.1, uses redis as session handler. Using Redis as a session handler benefits us in many ways. 
+* Sessions on native PHP are kept through files. Redis, on the other hand, keeps data in memory, making it easier to access. 
+* Information held with Redis is collected in a central area. In this way, all application nodes can share session information in this environment and session information can be preserved when switching between products.(Example: Youtube & Adsense)
+
+Additionally, there is two-factor authentication on the system.
+
+### What are the benefits of using two-factor authentication? <sub><sup>[source](https://uoa.custhelp.com/app/answers/detail/a_id/12689/~/benefits-of-using-two-factor-authentication#:~:text=Two%2Dfactor%20authentication%20works%20as,harder%20and%20reduce%20fraud%20risks.)</sup></sub>
+Two-factor authentication is an authentication mechanism to double check your identity is legitimate.
+
+When you want to sign into your account, you are prompted to authenticate with a username and a password - that's the first verification layer. Two-factor authentication works as an extra step in the process, a second security layer, that will re-confirm your identity. Its purpose is to make attackers' life harder and reduce fraud risks.
+
+If you already follow basic password security measures, two-factor authentication will make it more difficult for cyber criminals to breach your account because it is hard to get the second authentication factor, they would have to be much closer to you. This drastically reduces their chances to succeed.
+
+
+## Prerequisites
+* A PHP 8.1 web server running LAMP or LEMP on a server or your own device.
+* Where [Redis](https://redis.io/) is installed a server or your own device.
+
+## Requirments
+* PHP PDO Extension
+* PHP IMAP Extension
+* PHP [Redis](https://github.com/phpredis/phpredis) Extension
+* [Composer](https://getcomposer.org/download/)
+
+## Installing/Configuring
+..
+## Tests
+..
